a11a3698bf28cd4c104673aec367426f68ffa7a3
Anton <yushkevich.a.a@yandex.ru>
2024-07-09 00:13:40 +0300

    задача 3 выполнена

--- task8_3.py
+++ task8_3.py
@@ -1,9 +1,26 @@
-m = int(input())
-n = int(input())
+n = int(input("рыбаков: "))
 
-a = []
+w_b = int(input('грузоподъемность лодки: '))
 
+cnt = 1
+
+l1 = []
 
 for i in range(n):
-    w = int(input())
-    a.append(w)
+    r = int(input())
+    l1.append(r)
+
+l1.sort()
+
+w = 0
+
+for i in l1:
+    w += i
+    if w > w_b:
+        cnt += 1
+        w = i
+        
+
+print(cnt)
+
+
